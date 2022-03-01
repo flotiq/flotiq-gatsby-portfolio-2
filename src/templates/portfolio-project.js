b62ddcb0ce4d69330cf7ae6434e4e4ef87cdb024
Bartek Musiał <bartlomiej.musial@hotmail.com>
2022-03-01 11:46:52 +0100

    project description section build

--- src/templates/portfolio-project.js
+++ src/templates/portfolio-project.js
@@ -1,13 +1,33 @@
 import React from 'react';
 import { graphql } from 'gatsby';
-import { Header } from 'flotiq-components-react';
+import { Header, Paragraph, Image } from 'flotiq-components-react';
 import Layout from '../layouts/layout';
 
 const PortfolioProjectTemplate = ({ data }) => {
     const { project } = data;
     return (
         <Layout additionalClass={['bg-medium-gray md:bg-white']}>
-            <Header text={project.name} />
+            <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+                <div className="grid lg:grid-cols-3">
+                    <div className="pr-10 py-10 order-2 lg:order-1">
+                        <Header
+                            text={project.name}
+                            additionalClasses={['font-archivo uppercase tracking-widest !text-6xl '
+                            + 'mb-5 lg:mb-10 text-center lg:text-left']}
+                        />
+                        <Paragraph
+                            text={project.description}
+                            additionalClasses={['font-normal italic uppercase px-10 lg:px-0']}
+                        />
+                    </div>
+                    <div className="col-span-2 order-1 lg:order-2">
+                        <Image
+                            url={project.headerImage[0] && project.headerImage[0].localFile.publicURL}
+                            additionalClasses={['rounded-3xl w-full']}
+                        />
+                    </div>
+                </div>
+            </div>
         </Layout>
     );
 };
